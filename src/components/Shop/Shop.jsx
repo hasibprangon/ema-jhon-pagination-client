21badf7424488c94c2c4bdc0d1f04078702b67d8
hasibprangon <hasibulhossainp09@gmail.com>
2024-12-28 14:42:22 +0600

    calculate the number of pages and and button

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,24 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    const {count} = useLoaderData();
+    console.log(count);
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage)
+
+    // const pages = [];
+    // for(let i = 0; i< numberOfPages; i++) {
+    //     pages.push(i)
+    // };
+    // console.log(pages);
+
+    const pages = [...Array(numberOfPages).keys()];
+    console.log(pages);
 
     useEffect(() => {
         fetch('http://localhost:5000/products')
@@ -81,6 +94,11 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div>
+                {
+                    pages.map(page => <button className='pagination' key={page}>{page}</button>)
+                }
             </div>
         </div>
     );
